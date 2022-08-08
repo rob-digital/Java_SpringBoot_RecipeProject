712f14488a260993306a8ae53e16af99b41c4e02
rob-digital <robert.roksela@gmail.com>
2022-08-08 15:12:03 +0100

    Project initialised

---
+++ src/main/java/guru/spring/java_springboot_recipeproject/JavaSpringBootRecipeProjectApplication.java
@@ -0,0 +1,13 @@
+package guru.spring.java_springboot_recipeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootRecipeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringBootRecipeProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/spring/java_springboot_recipeproject/JavaSpringBootRecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.spring.java_springboot_recipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootRecipeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
